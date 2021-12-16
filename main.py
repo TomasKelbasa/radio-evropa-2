03d43458c109ac388fc141e1272f6445f607fc16
Tomáš Kelbasa <91247802+TomasKelbasa@users.noreply.github.com>
2021-12-16 11:39:09 +0100

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
+radio.set_transmit_power(3)
+radio.set_group(111)
+
+oldData = 0
+
 def on_forever():
     pass
 basic.forever(on_forever)
+
+def on_received_number(receivedNumber):
+    data = abs(radio.received_packet(RadioPacketProperty.SIGNAL_STRENGTH))
+    whaleysans.show_number(data)
+    if(data > 70):
+        music.play_tone(Note.C, music.beat())
+    elif(data > 60):
+        music.play_tone(Note.D, music.beat())
+    elif(data > 50):
+        music.play_tone(Note.E, music.beat())
+    elif(data > 40):
+        music.play_tone(Note.F, music.beat())
+    
+    
+    basic.pause(500)
+    oldData = data
+radio.on_received_number(on_received_number)
--- main.ts
+++ main.ts
@@ -1,3 +1,22 @@
+radio.setTransmitPower(3)
+radio.setGroup(111)
+let oldData = 0
 basic.forever(function on_forever() {
     
+})
+radio.onReceivedNumber(function on_received_number(receivedNumber: number) {
+    let data = Math.abs(radio.receivedPacket(RadioPacketProperty.SignalStrength))
+    whaleysans.showNumber(data)
+    if (data > 70) {
+        music.playTone(Note.C, music.beat())
+    } else if (data > 60) {
+        music.playTone(Note.D, music.beat())
+    } else if (data > 50) {
+        music.playTone(Note.E, music.beat())
+    } else if (data > 40) {
+        music.playTone(Note.F, music.beat())
+    }
+    
+    basic.pause(500)
+    let oldData = data
 })
